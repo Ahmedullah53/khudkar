ba0aacb65b0adc24c5f8dea22b893b5d8c61cc9f
Ahmedullah53 <45538283+Ahmedullah53@users.noreply.github.com>
2020-08-16 14:20:07 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./components/nav/nav";
+import Banner from "./components/banner";
+import Section from "./components/section";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner title="KHUDKAR" subText="Automate everything" />
+      <Section />
+      <Footer />
+    </div>
+  );
+}
