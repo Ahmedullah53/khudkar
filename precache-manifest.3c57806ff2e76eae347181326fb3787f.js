e95844ac61fb495da2ab979c2254e026cc02ff2d
Ahmedullah53 <ahmedullah53@gmail.com>
2020-08-17 05:33:10 +0500

    Updates

---
+++ precache-manifest.3c57806ff2e76eae347181326fb3787f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687a39fa4e22b172648b29165a37218f",
+    "url": "/khudkar/index.html"
+  },
+  {
+    "revision": "efb699a571cf5d5a840e",
+    "url": "/khudkar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9761ac47f2c294b29a8f",
+    "url": "/khudkar/static/js/2.f1651255.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/khudkar/static/js/2.f1651255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb699a571cf5d5a840e",
+    "url": "/khudkar/static/js/main.61627aed.chunk.js"
+  },
+  {
+    "revision": "e0518b9dbe8eae1939b2",
+    "url": "/khudkar/static/js/runtime-main.6378fbd2.js"
+  }
+]);
